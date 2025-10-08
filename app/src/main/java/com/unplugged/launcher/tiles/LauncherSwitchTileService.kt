3d7daaba3a057bd2330239549244969bbd0d4bf9
yodale <haurand@jungdruck.de>
2025-10-08 16:01:39 +0200

    added tile switcher

--- app/src/main/java/com/unplugged/launcher/tiles/LauncherSwitchTileService.kt
+++ app/src/main/java/com/unplugged/launcher/tiles/LauncherSwitchTileService.kt
@@ -1,16 +1,32 @@
 package com.unplugged.launcher.tiles
 
 import android.app.PendingIntent
+import android.app.role.RoleManager
+import android.app.role.RoleManager.ROLE_HOME
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.provider.Settings
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class LauncherSwitchTileService : TileService() {
 
+    private fun isCurrentLauncher(): Boolean {
+        val roleManager = getSystemService(RoleManager::class.java)
+        return roleManager.isRoleHeld(ROLE_HOME)
+    }
+
+    override fun onTileAdded() {
+        super.onTileAdded()
+        qsTile.updateTile()
+    }
     override fun onStartListening() {
-        updateTileLabel()
+        super.onStartListening()
+        qsTile.state = if (isCurrentLauncher()) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
+    }
+
+    override fun onStopListening() {
+        super.onStopListening()
     }
 
     override fun onClick() {
@@ -18,33 +34,8 @@ class LauncherSwitchTileService : TileService() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         }
         val pi = PendingIntent.getActivity(
-            this,
-            0,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE
+            this, 0, intent, PendingIntent.FLAG_IMMUTABLE
         )
         startActivityAndCollapse(pi)
-    }
-
-
-    private fun updateTileLabel() {
-        val pm = packageManager
-        val intent = Intent(Intent.ACTION_MAIN).apply {
-            addCategory(Intent.CATEGORY_HOME)
-        }
-
-        val resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
-        val currentLauncherPackage = resolveInfo?.activityInfo?.packageName
-
-        val myPackage = applicationContext.packageName
-
-        if (currentLauncherPackage == myPackage) {
-            qsTile.state = Tile.STATE_ACTIVE
-            qsTile.label = "Unplugged aktiv"
-        } else {
-            qsTile.state = Tile.STATE_INACTIVE
-            qsTile.label = "Launcher wechseln"
-        }
-        qsTile.updateTile()
     }
 }
