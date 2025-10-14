867dde984d1ea26eeb0afd2eabb9b7dfc49a15df
yodale <haurand@jungdruck.de>
2025-10-14 14:00:24 +0200

    added convertToGrayscale

---
+++ app/src/main/java/com/unplugged/launcher/data/LauncherApp.kt
@@ -0,0 +1,32 @@
+package com.unplugged.launcher.data
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.ColorFilter
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.Drawable
+
+data class LauncherApp(
+    val label: String,
+    val componentName: ComponentName,
+) {
+    fun loadIcon(context: Context): Drawable? {
+        return try {
+            val originalDrawable = context.packageManager.getActivityIcon(componentName)
+            val mutableDrawable = originalDrawable.mutate()
+            convertToGrayscale(mutableDrawable)
+        } catch (_: PackageManager.NameNotFoundException) {
+            null
+        }
+    }
+}
+
+private fun convertToGrayscale(drawable: Drawable): Drawable {
+    val matrix = ColorMatrix()
+    matrix.setSaturation(0f)
+    val filter: ColorFilter = ColorMatrixColorFilter(matrix)
+    drawable.colorFilter = filter
+    return drawable
+}
