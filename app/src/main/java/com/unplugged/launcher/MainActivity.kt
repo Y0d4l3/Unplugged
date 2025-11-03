58137180de6ff21bf755c1be33beb544e58701c8
yodale <haurand@jungdruck.de>
2025-11-03 11:56:11 +0100

    refactoring

--- app/src/main/java/com/unplugged/launcher/MainActivity.kt
+++ app/src/main/java/com/unplugged/launcher/MainActivity.kt
@@ -1,15 +1,11 @@
 package com.unplugged.launcher
 
-import android.content.ComponentName
-import android.content.pm.PackageManager
-import com.unplugged.launcher.service.NotificationStateService
-
-import com.unplugged.launcher.ui.components.HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.view.WindowCompat
+import com.unplugged.launcher.ui.components.HomeScreen
 import com.unplugged.launcher.ui.theme.UnpluggedTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,20 +21,5 @@ class MainActivity : ComponentActivity() {
                 HomeScreen()
             }
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        toggleNotificationService(enable = true)
-    }
-
-    private fun toggleNotificationService(enable: Boolean) {
-        val componentName = ComponentName(this, NotificationStateService::class.java)
-        val newState = if (enable) {
-            PackageManager.COMPONENT_ENABLED_STATE_ENABLED
-        } else {
-            PackageManager.COMPONENT_ENABLED_STATE_DISABLED
-        }
-        packageManager.setComponentEnabledSetting(componentName, newState, PackageManager.DONT_KILL_APP)
     }
 }
